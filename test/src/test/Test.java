5b3761b018bd1c826679c318b0078279f140f1c9
liat yevon cohen <liatcohen94@gmail.com>
2016-05-10 15:38:15 +0300

    first addition

---
+++ test/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import algorithms.demo.Demo;
+
+public class Test {
+		public static void main(String[]arg0)
+		{
+			System.out.println("liatc");
+			Demo temp = new Demo();
+			temp.run();
+		}
+
+	}
+
